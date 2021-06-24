8c9a4e11ce4dfe22935fc4f6ef6725e609041bda
Rian Ressai <rianeliel321@gmail.com>
2021-06-23 23:16:38 -0300

    configurações iniciais para mostrar o jogo

--- capitao.py
+++ capitao.py
@@ -1,5 +1,23 @@
+import pygame
+import random
+import time
+pygame.init()
 import os
 import time
+geloSound = pygame.mixer.Sound("assets/acertoGelo.wav")
+quedaSound = pygame.mixer.Sound("assets/letraCaindo.wav")
+icone = pygame.image.load("assets/capitaoIcon.ico.png")
+pygame.display.set_caption("Capitão América do Rian")
+pygame.display.set_icon(icone)
+largura = 800
+altura = 600
+display = pygame.display.set_mode((largura, altura))
+fps = pygame.time.Clock()
+fundo = pygame.image.load("assets/neve.png")
+capitao = pygame.image.load("assets/capitão.png")
+vogais = pygame.image.load("assets/vogais.png")
+preto = (0, 0, 0)
+branco = (255, 255, 255)
 def LimparTela():
     os.system("cls")
     return
